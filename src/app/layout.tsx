b38fb47b476a936c7fbfe4672e86c109bdee072d
DIBIA AI <dibia.aiagency@gmail.com>
2025-08-25 19:40:50 +0000

    start by implementing the floating button with the "breathing" animation

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,7 @@ import { AppFooter } from '@/components/layout/app-footer';
 import { SidebarProvider, SidebarInset } from '@/components/ui/sidebar';
 import { AppSidebar } from '@/components/app-sidebar';
 import { Inter } from 'next/font/google';
+import { FloatingChatButton } from '@/components/layout/floating-chat-button';
 
 const inter = Inter({
   subsets: ['latin'],
@@ -39,6 +40,7 @@ export default function RootLayout({
           </SidebarInset>
         </SidebarProvider>
         <Toaster />
+        <FloatingChatButton />
       </body>
     </html>
   );
